8791e71704478423f387a082545e34d1ec92a2cb
foxxjinn <foxx.jinn@gmail.com>
2020-07-11 23:16:07 -0600

    added routes

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express')
-const app = express()
 const PORT = process.env.PORT || 5000
+const connectDB = require('./config/db')
+
+const app = express()
+connectDB()
+
+// Middleware
+app.use(express.json({ extended: false }))
+
 
 app.get('/', (req, res) => {
     res.send('App Running')
 })
+
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 app.listen(PORT, ()=> {
     console.log(`Server started on port ${PORT}`)
