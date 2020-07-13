5c263d3fd5002479d5581ddad245708434c5386c
foxxjinn <foxx.jinn@gmail.com>
2020-07-12 20:33:58 -0600

    comments added, small refactoring

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-const express = require('express')
-const PORT = process.env.PORT || 5000
-const connectDB = require('./config/db')
+/////////////////
+/*
+    Author: Jinn Foxx, foxxjinn@gmail.com
+    NodeJS Express Server
+    connects to MonogoDB for database
+*/
+/////////////////
 
+const express = require('express')
 const app = express()
+const connectDB = require('./config/db')
 connectDB()
 
 // Middleware
 app.use(express.json({ extended: false }))
 
-
+// Homepage
 app.get('/', (req, res) => {
     res.send('App Running')
 })
 
+// Routes
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 
+// Start Server
+const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> {
     console.log(`Server started on port ${PORT}`)
 })
